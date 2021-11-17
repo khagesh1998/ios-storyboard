410b860f3d6a11af744d96db966465909c9671e5
khagesh1998 <khagesh.kumar@dream11.com>
2021-11-18 00:44:44 +0530

    feat: click to increase

--- test2/ViewController.swift
+++ test2/ViewController.swift
@@ -9,6 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var display: UILabel!
+    
+    var score = 0
+    
+    
+    @IBAction func addButton(_ sender: UIButton) {
+        print("clicked")
+        score+=1
+        display.text = String(score)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
